42b2277bf23907ce74d6e4c6b414351985c96ca9
IliasChekkori <ilc224@lehigh.edu>
2021-07-02 10:04:23 -0400

    wait conn

--- RcControl.py
+++ RcControl.py
@@ -10,7 +10,26 @@ import time
 
 # Create the connection
 master = mavutil.mavlink_connection('udpout:0.0.0.0:9000')
+wait_conn()
 # Wait a heartbeat before sending commands
+
+
+def wait_conn():
+    """
+    Sends a ping to stabilish the UDP communication and awaits for a response
+    """
+    msg = None
+    while not msg:
+        master.mav.ping_send(
+            int(time.time() * 1e6),  # Unix time in microseconds
+            0,  # Ping number
+            0,  # Request ping of all systems
+            0  # Request ping of all components
+        )
+        msg = master.recv_match()
+        time.sleep(0.5)
+
+
 master.wait_heartbeat()
 
 for i in range(20):
