6660ebcf853c6044004b957c21293832aa8b1c71
Zemichael <zemichaeldamtew@gmail.com>
2021-07-13 07:43:49 -0700

    Add depth control

---
+++ Depth_control.py
@@ -0,0 +1,58 @@
+import sys
+import time
+import math
+# Import mavutil
+from pymavlink import mavutil
+from pymavlink.mavextra import altitude
+# Imports for attitude
+from pymavlink.quaternion import QuaternionBase
+
+master = mavutil.mavlink_connection('udpout:0.0.0.0:9000')
+
+boot_time = time.time()
+
+def wait_conn():
+    """
+    Sends a ping to stabilish the UDP communication and awaits for a response
+    """
+    msg = None
+    while not msg:
+        master.mav.ping_send(
+            int(time.time() * 1e6),  # Unix time in microseconds
+            0,  # Ping number
+            0,  # Request ping of all systems
+            0  # Request ping of all components
+        )
+        msg = master.recv_match()
+        time.sleep(0.5)
+
+
+# arm ArduSub autopilot and wait until confirmed
+master.arducopter_arm()
+master.motors_armed_wait()
+
+def set_target_depth(depth):
+    """ Sets the target depth while in depth-hold mode.
+
+    Uses https://mavlink.io/en/messages/common.html#SET_POSITION_TARGET_GLOBAL_INT
+
+    'depth' is technically an altitude, so set as negative meters below the surface
+        -> set_target_depth(-1.5) # sets target to 1.5m below the water surface.
+
+    """
+    master.mav.set_position_target_global_int_send(
+        int(1e3 * (time.time() - boot_time)), # ms since boot
+        master.target_system, master.target_component,
+        coordinate_frame=mavutil.mavlink.MAV_FRAME_GLOBAL_INT,
+        type_mask=0xdfe,  # ignore everything except z position
+        lat_int=0, lon_int=0, alt=depth, # (x, y WGS84 frame pos - not used), z [m]
+        vx=0, vy=0, vz=0, # velocities in NED frame [m/s] (not used)
+        afx=0, afy=0, afz=0, yaw=0, yaw_rate=0
+        # accelerations in NED frame [N], yaw, yaw_rate
+        #  (all not supported yet, ignored in GCS Mavlink)
+    )
+
+set_target_depth(-1)
+set_target_depth(1)
+master.arducopter_disarm()
+master.motors_disarmed_wait()
