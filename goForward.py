ae2f34d3e5d9aa34f52dbb650092ad530be34bf2
IliasChekkori <ilc224@lehigh.edu>
2021-07-22 10:54:06 -0400

    as

---
+++ goForward.py
@@ -0,0 +1,59 @@
+from pymavlink import mavutil
+import time
+import argparse
+import math
+
+
+# Create the connection
+
+# Wait a heartbeat before sending commands
+# master = mavutil.mavlink_connection(udpin:0.0.0.0:14550)
+master = mavutil.mavlink_connection('udpout:0.0.0.0:9000')
+
+
+def wait_conn():
+    """
+    Sends a ping to stabilish the UDP communication and awaits for a response
+    """
+    msg = None
+    while not msg:
+        master.mav.ping_send(
+            int(time.time() * 1e6),  # Unix time in microseconds
+            0,  # Ping number
+            0,  # Request ping of all systems
+            0  # Request ping of all components
+        )
+        msg = master.recv_match()
+        time.sleep(0.5)
+
+
+def manualControl(x, y, z):
+    mode = 'MANUAL'
+    mode_id = master.mode_mapping()[mode]
+    master.mav.set_mode_send(
+        master.target_system,
+        mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
+        mode_id)
+
+    master.mav.manual_control_send(
+        master.target_system,
+        x,  # x
+        y,  # y
+        z,  # z
+        0,  # r
+        0)  # buttons
+
+
+wait_conn()
+print("<<<<<<CONNECTION ESTABLISHED>>>>>>")
+boot_time = time.time()
+master.wait_heartbeat()
+print("<<<<<<<HEARTBEAT RECEIVED>>>>>>")
+
+
+master.arducopter_arm()
+time.sleep(1)
+print("<<<<<<ARMED>>>>>>")
+
+while True:
+    manualControl(10000, 0, 0)
